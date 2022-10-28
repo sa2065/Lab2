18b5857ab7f7ea212a0487adec28870fe2f8f32b
sa2065 <114130548+sa2065@users.noreply.github.com>
2022-10-28 22:29:31 +0400

    Initial commit

---
+++ game.js
@@ -0,0 +1,17 @@
+function Bear() {
+  this.dBear = 100;
+  this.htmlElement = document.getElementById("bear");
+  this.id = this.htmlElement.id;
+  this.x = this.htmlElement.offsetLeft;
+  this.y = this.htmlElement.offsetTop;
+  this.move = function (xDir, yDir) {
+    this.x += this.dBear * xDir;
+    this.y += this.dBear * yDir;
+    this.display();
+  };
+  this.display = function () {
+    this.htmlElement.style.left = this.x + "px";
+    this.htmlElement.style.top = this.y + "px";
+    this.htmlElement.style.display = "absolute";
+  };
+}
